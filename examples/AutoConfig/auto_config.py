3302064e618cd505e1a917ec6e4e18c0295fc800
xiaol-luo <xiaol.luo@163.com>
2017-08-09 10:02:09 +0800

    modify auto config code

--- examples/AutoConfig/auto_config.py
+++ examples/AutoConfig/auto_config.py
@@ -9,19 +9,8 @@ from config_list import ConfigListDescript
 log = logging
 
 def run(cfg_path):
-    if not os.path.exists(cfg_path) or not os.path.isfile(cfg_file):
-        log.error(cfg_path + " not exist")
-        sys.exit(1)
-    env_desc = None
-    with codecs.open(cfg_path, "r") as f:
-        cfg_praser = configparser.ConfigParser()
-        cfg_praser.read_file(f)
-        env_desc = ConfigListDescript()
-        ret = env_desc.Init(cfg_praser)
-        if not ret:
-            log.error("EnvDescript Init Fail")
-            sys.exit(1) 
-            
+    env_desc = ConfigListDescript.load(cfg_path)
+        
 
 if __name__ == "__main__":
     log.basicConfig(level=logging.DEBUG)
--- examples/AutoConfig/config_list.py
+++ examples/AutoConfig/config_list.py
@@ -1,4 +1,7 @@
 import configparser
+import re
+import os
+import codecs
 
 STRING_EMPTY = ""
 
@@ -14,15 +17,16 @@ class Excel2CsvDescript(object):
         self.out_lua_file_path = STRING_EMPTY
         return super().__init__(**kwargs)
     
-    def Init(self, cfg_praser):
-        file_path = env_section["file_path"]
-        sheet_name = env_section["sheet_name"]
-        out_csv_file_path = env_section["out_csv_file_path"]
-        class_name = env_section["class_name"]
-        out_cs_file_path = env_section["out_cs_file_path"]
-        out_cpp_file_path = env_section["out_cpp_file_path"]
-        out_lua_file_path = env_section["out_lua_file_path"]
+    def Init(self, cfg_section):
+        self.file_path = cfg_section["file_path"]
+        self.sheet_name = cfg_section["sheet_name"]
+        self.out_csv_file_path = cfg_section["out_csv_file_path"]
+        self.class_name = cfg_section["class_name"]
+        self.out_cs_file_path = cfg_section["out_cs_file_path"]
+        self.out_cpp_file_path = cfg_section["out_cpp_file_path"]
+        self.out_lua_file_path = cfg_section["out_lua_file_path"]
         return True
+
 
 class ConfigListDescript(object):
     ENV_SECTION = "env"
@@ -41,16 +45,28 @@ class ConfigListDescript(object):
         if not cfg_praser.has_section(ConfigListDescript.ENV_SECTION):
             return False
         env_section = cfg_praser[ConfigListDescript.ENV_SECTION]
-        excel_dir = env_section["excel_dir"]
-        out_config_dir = env_section["out_config_dir"]
-        out_code_dir = env_section["out_code_dir"]
+        self.excel_dir = env_section["excel_dir"]
+        self.out_config_dir = env_section["out_config_dir"]
+        self.out_code_dir = env_section["out_code_dir"]
         self.excel2csv_descs = []
-        for section_name in cfg_praser.sections:
+        for section_name in cfg_praser.sections():
             if not re.match(ConfigListDescript.EXCEL2CSV_REGEX_PATTERN, section_name):
                 continue
-            excel2csv_desc = Excel2CsvDescript()
+            excel2csv_desc = Excel2CsvDescript(self)
             if not excel2csv_desc.Init(cfg_praser[section_name]):
                 log.error("cfg_path - {0} {1} error {2}".format(cfg_path, section_name, cfg_praser.sections[section_name]))
             else:
                 self.excel2csv_descs.append(excel2csv_desc)
         return True
+
+    @staticmethod
+    def load(file_path):
+        env_desc = None
+        if os.path.exists(file_path) and os.path.isfile(file_path): 
+            with codecs.open(file_path, "r") as f:
+                cfg_praser = configparser.ConfigParser()
+                cfg_praser.read_file(f)
+                env_desc = ConfigListDescript()
+                if not env_desc.Init(cfg_praser):
+                    env_desc = None
+        return env_desc
