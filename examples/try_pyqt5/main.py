2725174a7929a0d1b1642e395b34b78c59f5bd62
xiaol-luo <xiaol.luo@163.com>
2021-05-26 20:25:39 +0800

    modify

--- examples/try_pyqt5/main.py
+++ examples/try_pyqt5/main.py
@@ -6,13 +6,21 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog
 class MainDialog(QDialog):
     def __init__(self):
         super().__init__()
-        import ui.test_dialog
-        self.ui = ui.test_dialog.Ui_Dialog()
+        import qt_ui.main_dialog
+        self.ui = qt_ui.main_dialog.Ui_Dialog()
         self.ui.setupUi(self)
         self.ui.ok.clicked.connect(self._clicked_ok)
     
     def _clicked_ok(self):
         print("clicked ok")
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        import qt_ui.main_window
+        self.ui = qt_ui.main_dialog.main_window.Ui_MainWindow()
+        self.ui.setupUi(self)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- examples/try_pyqt5/qt_ui/main_dialog.py
+++ examples/try_pyqt5/qt_ui/main_dialog.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'untitled.ui'
+# Form implementation generated from reading ui file 'main_dialog.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.4
 #
