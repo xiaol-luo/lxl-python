4e91f4ac6677267abdcc747f394e02737675c54d
xiaol-luo <xiaol.luo@163.com>
2021-11-16 00:52:06 +0800

    modify

---
+++ codes/examples/try_paramiko/try_paramiko.py
@@ -0,0 +1,12 @@
+
+import paramiko
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(hostname="119.28.224.75", port=22, username="root", password="*2012xiaolzz")
+# 执行命令
+stdin, stdout, stderr = ssh.exec_command('df -hl')
+# 结果放到stdout中，如果有错误将放到stderr中
+print(stdout.read().decode())
+# 关闭连接
+ssh.close()
