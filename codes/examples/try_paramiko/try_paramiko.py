eeed8001ec0c9625d7da2af271d071791b2b2a3e
xiaol-luo <xiaol.luo@163.com>
2021-11-16 00:52:53 +0800

    modify

--- codes/examples/try_paramiko/try_paramiko.py
+++ codes/examples/try_paramiko/try_paramiko.py
@@ -3,7 +3,7 @@ import paramiko
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(hostname="119.28.224.75", port=22, username="root", password="*2012xiaolzz")
+ssh.connect(hostname="119.28.224.75", port=22, username="root", password="")
 # 执行命令
 stdin, stdout, stderr = ssh.exec_command('df -hl')
 # 结果放到stdout中，如果有错误将放到stderr中
