15646500fd6bb94f0718fef5fcd36d52c9db1c9e
xiaol-luo <xiaol.luo@163.com>
2021-11-16 19:50:18 +0800

    modify

--- codes/examples/try_paramiko/try_paramiko.py
+++ codes/examples/try_paramiko/try_paramiko.py
@@ -1,12 +1,36 @@
 
 import paramiko
+import os
+
+hostname = "119.28.224.75"
+port = 22
+username = "root"
+password = ""
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(hostname="119.28.224.75", port=22, username="root", password="")
+ssh.connect(hostname=hostname, port=port, username=username, password=password)
+
 # 执行命令
-stdin, stdout, stderr = ssh.exec_command('df -hl')
+# stdin, stdout, stderr = ssh.exec_command('df -hl')
 # 结果放到stdout中，如果有错误将放到stderr中
-print(stdout.read().decode())
+# print(stdout.read().decode())
+
+t = paramiko.Transport(hostname, port)
+t.connect(username=username, password=password)
+sftp = paramiko.SFTPClient.from_transport(t)
+local_dir = "E:/git/code/lock_step/client/game_card/Assets/StreamingAssets/Abs"
+remote_dir = "/usr/share/nginx/download/Abs"
+
+ssh.exec_command("mkdir -p {0}".format(remote_dir))
+
+dir_entities = os.listdir(local_dir)
+print("dir_entities", dir_entities)
+for file in dir_entities:
+    sftp.put(os.path.join(local_dir, file), os.path.join(remote_dir, file).replace("\\", "/"))
+
+ssh.exec_command("chmod -R 755 {0}".format(remote_dir))
+
 # 关闭连接
 ssh.close()
+sftp.close()
