c4eb20cd76c2a11962397a5f64af45dc865b93de
xiaol-luo <xiaol.luo@163.com>
2022-01-24 19:44:03 +0800

    modify

--- codes/examples/try_bottle/bottle_gevent.py
+++ codes/examples/try_bottle/bottle_gevent.py
@@ -1,3 +1,5 @@
+import sys
+
 from gevent import monkey; monkey.patch_all()
 import collections
 import queue
@@ -14,12 +16,6 @@ import typing
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
-
-wait_execute_fns = collections.deque()
-
-
-def delay_execute(fn):
-    wait_execute_fns.append(fn)
 
 
 class WebApp(object):
@@ -81,58 +77,13 @@ class WebApp(object):
         self.web_ins.bt.route("/download/<file_name:path>", callback=self.test_download)
 
 
-FnParam = typing.ParamSpec("FnParam")
-FnRet = typing.Optional[typing.List[typing.Any]]
-
-
-class Task(object):
-    fn: typing.Callable[[FnParam], FnRet]
-    cb_fn: typing.Callable[[typing.Concatenate[bool, FnParam]], typing.NoReturn]
-    fn_execute_succ: bool
-    fn_execute_rets: FnRet
-    fn_args: FnParam.args
-    fn_kwargs: FnParam.kwargs
-
-    def __init__(self, cb_fn, fn, *args, **kwargs):
-        super(Task, self).__init__()
-        self.fn = fn
-        assert (callable(self.fn))
-        self.fn_args = args
-        self.fn_kwargs = kwargs
-        self.cb_fn = cb_fn
-        self.fn_execute_succ = None
-        self.fn_execute_rets = None
-
-    def do_logic(self):
-        try:
-            ret = self.fn(*self.fn_args, **self.fn_kwargs)
-            if ret is not None:
-                self.fn_execute_rets = ret
-            self.fn_execute_succ = True
-        except Exception as e:
-            self.fn_execute_succ = False
-            self.fn_execute_rets = [str(e)]
-
-    def use_result(self):
-        if callable(self.cb_fn):
-            if self.fn_execute_rets is not None:
-                self.cb_fn(self.fn_execute_succ, *self.fn_execute_rets)
-            else:
-                self.cb_fn(self.fn_execute_succ)
-
-
 class WebInstance(object):
     def __init__(self, bt):
         super(WebInstance, self).__init__()
         self.bt = bt
         self.app = None
-        self.task_lock = threading.Lock()
-        self.task_result_lock = threading.Lock()
-        self.task_list_in_thread = []
-        self.task_list_in_main = []
-        self.done_task_list = []
-        self.task_thread = None
-        self.process_task_is_exist = False
+        from codes.libs.thread_task_mgr.thread_safe_task_mgr import ThreadSafeTaskMgr
+        self.task_mgr = ThreadSafeTaskMgr()
 
     def set_app(self, app):
         if self.app:
@@ -142,60 +93,13 @@ class WebInstance(object):
             self.app.bind_urls()
 
     def add_task(self, cb_fn, fn, *args, **kwargs):
-        print("add_task")
-        self.task_lock.acquire()
-        task = Task(cb_fn, fn, args, kwargs)
-        self.task_list_in_main.append(task)
-        self.task_lock.release()
+        self.task_mgr.add_task(cb_fn, fn, *args, **kwargs)
 
     def process_task_logic(self):
-        sleep_ev = threading.Event()
-        while not self.process_task_is_exist:
-            need_sleep = False
-            if self.task_lock.acquire(False):
-                if len(self.task_list_in_main) <= 0:
-                    need_sleep = True
-                else:
-                    tmp = self.task_list_in_thread
-                    self.task_list_in_thread = self.task_list_in_main
-                    self.task_list_in_main = tmp
-                self.task_lock.release()
-            else:
-                need_sleep = True
-            if not need_sleep:
-                print("need_sleep:{0}".format(self.task_list_in_thread))
-            if need_sleep:
-                sleep_ev.wait(0.01)
-            else:
-                for task in self.task_list_in_thread:
-                    print("task.do_logic() start")
-                    task.do_logic()
-                    print("task.do_logic() end")
-                if self.task_result_lock.acquire():
-                    self.done_task_list.extend(self.task_list_in_thread)
-                    self.task_list_in_thread.clear()
-                    self.task_result_lock.release()
+        self.task_mgr.process_task_logic()
 
     def process_task_result(self):
-        sleep_ev = threading.Event()
-        while not self.process_task_is_exist:
-            need_sleep = True
-            done_task_list = None
-            if self.task_result_lock.acquire(False):
-                if len(self.done_task_list) > 0:
-                    need_sleep = False
-                    done_task_list = self.done_task_list
-                    self.done_task_list = []
-                self.task_result_lock.release()
-            if not need_sleep:
-                if done_task_list:
-                    for task in done_task_list:
-                        task.use_result()
-            else:
-                sleep_ev.wait(0.01)
-
-
-# bottle.run(host='0.0.0.0', port=8080, server='gevent')
+        self.task_mgr.process_task_result()
 
 if __name__ == '__main__':  # pragma: no coverage
     print("222")
---
+++ codes/examples/try_bottle/run_bottle_gevent.bat
@@ -0,0 +1,4 @@
+rem 设置PYTHONPATH为项目所在目录
+SET PYTHONPATH=D:\work_space\code\lxl-python 
+rem 在此目录下执行下边的脚本 D:\work_space\code\lxl-python\codes\examples\try_bottle
+python bottle_gevent.py
