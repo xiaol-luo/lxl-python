4bc57814659ce79fad722eff5c11fee2761a65ed
xiaol-luo <xiaol.luo@163.com>
2022-05-26 23:37:40 +0800

    rename directory codes to script_root

---
+++ script_root/examples/gen_tcy_server_cfg/data/out/zone_1/etcd/enable_auth.py
@@ -0,0 +1,93 @@
+# machine Machine
+# etcd_cluster EtcdServerCluster
+# etcd EtcdServer
+import paramiko
+import typing
+import random
+import sys
+import os
+
+class IndentFlag(object):
+    def __enter__(self):
+        pass
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
+
+
+ListOrStr = typing.TypeVar("ListOrStr", typing.List[str], str)
+
+
+def paramiko_ssh_cmd(ssh_client: paramiko.SSHClient, cmd: ListOrStr, exit_when_error:bool=False):
+    shell: paramiko.Channel = ssh_client.invoke_shell()
+    if isinstance(cmd, str):
+        shell.sendall(cmd + "\n")
+    else:
+        for e in cmd:
+            shell.sendall(e + "\n")
+    shell.sendall("exit $?\n")
+    exit_status = shell.recv_exit_status()
+    out_lines = []
+    while True:
+        recv_data = shell.recv(1024)
+        if len(recv_data) > 0:
+            out_lines.append(recv_data.decode("utf-8"))
+        else:
+            break
+    error_lines = []
+    while True:
+        recv_data = shell.recv_stderr(1024)
+        if len(recv_data) > 0:
+            error_lines.append(recv_data.decode("utf-8"))
+        else:
+            break
+    if exit_when_error:
+        print("paramiko_ssh_cmd fail, exit_code is {0}\n out is {1}\n error is {2}".format(exit_status, "".join(out_lines), "".join(error_lines)))
+        sys.exit(exit_status)
+    return exit_status, "".join(out_lines), "".join(error_lines)
+
+
+ssh_client = None
+with IndentFlag():
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.connect(hostname="119.91.239.128", port="22", username="root", \
+        pkey=paramiko.RSAKey.from_private_key_file(r"C:/Users/luoxiaolong/.ssh/keys/root/id_rsa", "xiaolzz"))
+
+
+with IndentFlag():
+    # run docker container
+    import random
+    ct_name = "ct_{}".format(random.randint(1, 99999999))
+    opt_mount_volumes = []
+    opt_mount_volumes.append("--mount type=volume,src=tcy_zone,dst=/root/zone")
+    opt_network = "--network my-network"
+    run_cmd = "docker run -itd --name {name} {network} {mount_volumes} {image} {command}".format(
+        name=ct_name, network=opt_network,  mount_volumes=" ".join(opt_mount_volumes), image="lxl_debian", command="/bin/bash")
+    ret, out_txt, error_txt = paramiko_ssh_cmd(ssh_client, run_cmd)
+    if 0 != ret:
+        print("docker exec: run docker container fail, exit_code is {0}\nstd_out is {1}\nstd_error is {2}\n-------------\n".format(ret, out_txt, error_txt))
+        sys.exit(ret)
+    # execute cmds in docker contianer
+    ret, out_txt, error_txt = paramiko_ssh_cmd(ssh_client, "docker exec {name} {command}".format(name=ct_name, command="/bin/bash -c 'echo xiaolzz | etcdctl --endpoints //10.0.1.190:2379,//10.0.1.191:2379,//10.0.1.192:2379 user add root' "))
+    if 0 != ret:
+        print("docker exec: run cmd fail, exit_code is {0}\nstd_out is {1}\nstd_error is {2}\n-------------\n".format(ret, out_txt, error_txt))
+    ret, out_txt, error_txt = paramiko_ssh_cmd(ssh_client, "docker exec {name} {command}".format(name=ct_name, command="etcdctl --endpoints //10.0.1.190:2379,//10.0.1.191:2379,//10.0.1.192:2379 --username root:xiaolzz auth enable"))
+    if 0 != ret:
+        print("docker exec: run cmd fail, exit_code is {0}\nstd_out is {1}\nstd_error is {2}\n-------------\n".format(ret, out_txt, error_txt))
+    ret, out_txt, error_txt = paramiko_ssh_cmd(ssh_client, "docker exec {name} {command}".format(name=ct_name, command="/bin/bash -c  'echo zone_1 | etcdctl --endpoints //10.0.1.190:2379,//10.0.1.191:2379,//10.0.1.192:2379 --username root:xiaolzz user add zone_1' "))
+    if 0 != ret:
+        print("docker exec: run cmd fail, exit_code is {0}\nstd_out is {1}\nstd_error is {2}\n-------------\n".format(ret, out_txt, error_txt))
+    ret, out_txt, error_txt = paramiko_ssh_cmd(ssh_client, "docker exec {name} {command}".format(name=ct_name, command="etcdctl --endpoints //10.0.1.190:2379,//10.0.1.191:2379,//10.0.1.192:2379 --username root:xiaolzz role add rw_all"))
+    if 0 != ret:
+        print("docker exec: run cmd fail, exit_code is {0}\nstd_out is {1}\nstd_error is {2}\n-------------\n".format(ret, out_txt, error_txt))
+    ret, out_txt, error_txt = paramiko_ssh_cmd(ssh_client, "docker exec {name} {command}".format(name=ct_name, command="etcdctl --endpoints //10.0.1.190:2379,//10.0.1.191:2379,//10.0.1.192:2379 --username root:xiaolzz role grant --readwrite --path / rw_all"))
+    if 0 != ret:
+        print("docker exec: run cmd fail, exit_code is {0}\nstd_out is {1}\nstd_error is {2}\n-------------\n".format(ret, out_txt, error_txt))
+    ret, out_txt, error_txt = paramiko_ssh_cmd(ssh_client, "docker exec {name} {command}".format(name=ct_name, command="etcdctl --endpoints //10.0.1.190:2379,//10.0.1.191:2379,//10.0.1.192:2379 --username root:xiaolzz user grant --roles rw_all zone_1"))
+    if 0 != ret:
+        print("docker exec: run cmd fail, exit_code is {0}\nstd_out is {1}\nstd_error is {2}\n-------------\n".format(ret, out_txt, error_txt))
+    # remove docker container
+    paramiko_ssh_cmd(ssh_client, [
+        "docker container kill {0}".format(ct_name),
+        "docker container prune -f",
+    ])
---
+++ script_root/examples/gen_tcy_server_cfg/render/render_etcd.py
@@ -0,0 +1,91 @@
+import tt
+import typing
+import script_root.libs.utils.file_utils as file_utils
+import os
+import pathlib
+
+from . import export_file
+
+
+def export_cluster_start_file(out_root_dir:str, zone:typing.Dict[str, typing.Dict[str, str]], etcd_cluster):
+    for etcd in etcd_cluster.server_list.values():
+        tt_ret, tt_content = tt.render("etcd/etcd_start.py.j2", zone=zone, etcd=etcd, etcd_cluster=etcd_cluster)
+        out_file = export_file.cal_etcd_start_file_path(out_root_dir, zone, etcd)
+        os.makedirs(os.path.dirname(out_file), exist_ok=True)
+        if tt_ret:
+            file_utils.write_file(out_file, tt_content)
+
+
+def export_cluster_stop_file(out_root_dir:str, zone:typing.Dict[str, typing.Dict[str, str]], etcd_cluster):
+    for etcd in etcd_cluster.server_list.values():
+        tt_ret, tt_content = tt.render("etcd/etcd_stop.py.j2", zone=zone, etcd=etcd, etcd_cluster=etcd_cluster)
+        out_file = export_file.cal_etcd_stop_file_path(out_root_dir, zone, etcd)
+        os.makedirs(os.path.dirname(out_file), exist_ok=True)
+        if tt_ret:
+            file_utils.write_file(out_file, tt_content)
+
+
+def export_cluster_clear_file(out_root_dir:str, zone:typing.Dict[str, typing.Dict[str, str]], etcd_cluster):
+    for etcd in etcd_cluster.server_list.values():
+        tt_ret, tt_content = tt.render("etcd/etcd_clear.py.j2", zone=zone, etcd=etcd, etcd_cluster=etcd_cluster)
+        out_file = export_file.cal_etcd_clear_file_path(out_root_dir, zone, etcd)
+        os.makedirs(os.path.dirname(out_file), exist_ok=True)
+        if tt_ret:
+            file_utils.write_file(out_file, tt_content)
+
+
+def export_cluster_logs_file(out_root_dir:str, zone:typing.Dict[str, typing.Dict[str, str]], etcd_cluster):
+    for etcd in etcd_cluster.server_list.values():
+        tt_ret, tt_content = tt.render("etcd/etcd_logs.py.j2", zone=zone, etcd=etcd, etcd_cluster=etcd_cluster)
+        out_file = export_file.cal_etcd_logs_file_path(out_root_dir, zone, etcd)
+        os.makedirs(os.path.dirname(out_file), exist_ok=True)
+        if tt_ret:
+            file_utils.write_file(out_file, tt_content)
+
+
+def export_cluster_opera_file(out_root_dir:str, zone:typing.Dict[str, typing.Dict[str, str]], etcd_cluster, opera, python_path=None):
+    if not python_path:
+        python_path = "python"
+    else:
+        python_path = pathlib.Path(python_path).as_posix()
+    script_list = []
+    for etcd in etcd_cluster.server_list.values():
+        if "start" == opera:
+            script_list.append(export_file.cal_etcd_start_file_path(out_root_dir, zone, etcd))
+        if "stop" == opera:
+            script_list.append(export_file.cal_etcd_stop_file_path(out_root_dir, zone, etcd))
+        if "clear" == opera:
+            script_list.append(export_file.cal_etcd_clear_file_path(out_root_dir, zone, etcd))
+        if "logs" == opera:
+            script_list.append(export_file.cal_etcd_logs_file_path(out_root_dir, zone, etcd))
+    tt_ret, tt_content = tt.render("etcd/etcd_cluster_opera.py.j2", script_list=script_list, python_path=python_path)
+    out_file = export_file.cal_etcd_cluster_opera_file_path(out_root_dir, zone, "{0}_cluster.py".format(opera))
+    os.makedirs(os.path.dirname(out_file), exist_ok=True)
+    if tt_ret:
+        file_utils.write_file(out_file, tt_content)
+
+
+def export_cluster_etcdctl_cmds_file(out_root_dir:str, zone:typing.Dict[str, typing.Dict[str, str]], etcd_cluster, out_file:str, cmd_list:typing.List[str], is_auth=False):
+    for etcd in etcd_cluster.server_list.values():
+        tt_ret, tt_content = tt.render("etcd/etcdctl_cmds.py.j2", zone=zone, etcd=etcd, etcd_cluster=etcd_cluster, cmd_list=cmd_list, is_auth=is_auth)
+        save_file = export_file.cal_etcd_etcdctl_cmds_file_path(out_root_dir, zone, out_file)
+        os.makedirs(os.path.dirname(save_file), exist_ok=True)
+        if tt_ret:
+            file_utils.write_file(save_file, tt_content)
+        # 一个就够了
+        break
+
+
+def export_cluster_enable_auth_file(out_root_dir:str, zone:typing.Dict[str, typing.Dict[str, str]], etcd_cluster):
+    for etcd in etcd_cluster.server_list.values():
+        tt_ret, tt_content = tt.render("etcd/etcdctl_enable_auth.py.j2", zone=zone, etcd=etcd, etcd_cluster=etcd_cluster)
+        save_file = export_file.cal_etcd_enable_auth_file_path(out_root_dir, zone)
+        os.makedirs(os.path.dirname(save_file), exist_ok=True)
+        if tt_ret:
+            file_utils.write_file(save_file, tt_content)
+        # 一个就够了
+        break
+
+
+
+
