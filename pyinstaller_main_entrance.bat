30b9b7af86de700403e3444d306fc44b7f5a3639
xiaol-luo <xiaol.luo@163.com>
2022-10-13 00:35:48 +0800

    pyinstaller --add-datas, if use dir_xx/*;dir_xx, then all files in sub dirs will not in their own dir, but all files put in dir_xx

--- pyinstaller_main_entrance.bat
+++ pyinstaller_main_entrance.bat
@@ -2,4 +2,4 @@ pyinstaller.exe -F script_root/main_entrance.py  ^
 --collect-submodules script_root/examples/gen_lua_config ^
 -p script_root ^
 --hidden-import examples.gen_lua_config.main ^
---add-data  "script_root/examples/gen_lua_config/impl/tt/templates/*;examples/gen_lua_config/impl/tt/templates"
+--add-data  "script_root/examples/gen_lua_config/impl/tt/templates;examples/gen_lua_config/impl/tt/templates"
